df8f5b8640b4fc156031b0bd6027f169d9177df5
AlekseyBelyaev2606 <partchoice44@gmail.com>
2022-07-02 00:08:19 +0300

    second commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,39 +1,31 @@
 fun main() {
 
     var agoToText: Int = 0
-
-
     var rightSecText: String
-    var agoToTextMin: Int
+    var agoToTextMin: Int = (agoToText/60)  % 100
 
-    fun RightMin (){
-        agoToTextMin = agoToText  % 10
-        if ((agoToTextMin == 1) && (agoToText % 100 != 11)){
-            rightSecText = "минуту"
-        }
-        if ((for (agoToTextMin) in (2..4))){
-            rightSecText = "минуты"
-        }
-        else{
-            rightSecText = "минут"
+    var RightMin = when (agoToTextMin){
+            1, 21, 31, 41, 51 -> "минуту"
+            2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54 -> "минуты"
+            else -> "минут"
     }
 
-        var agoToTextHourPer = agoToText % 24
-        var RightHour = when (agoToTextHourPer) {
+    var agoToTextHourPer = agoToText/60/24
+    var RightHour = when (agoToTextHourPer) {
             1, 21 -> "час"
             2, 3, 4, 22, 23, 24 -> "часа"
             else -> "часов"
-        }
+    }
 
-        var rightAnswer = when (agoToText) {
+    var rightAnswer = when {
             agoToText > 60 -> "только что"
-            (agoToText > 61) && (agoToText < 60 * 60) -> RightMin()
+            (agoToText > 61) && (agoToText < 60 * 60) -> RightMin
             (agoToText > (60*60+1)) && (agoToText < 60*60*24) -> RightHour
             (agoToText > (24*60*60+1)) && (agoToText < 48*60*60) -> "вчера"
             (agoToText > (72*60*60+1)) -> "давно"
-
-        }
+            else -> "давно"
     }
+}
 
 
 
